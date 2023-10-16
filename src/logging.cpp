e2a54da3b77d548dc823c0a8593e9e4fa6fb2799
Marek Zeleny <zeleny.marek@seznam.cz>
2023-10-16 12:30:42 +0200

    Finish and improve logging

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,13 +1,15 @@
 #include "logging.hpp"
 
 #include <sstream>
+#include <chrono>
+#include <iomanip>
 
 namespace dp {
 
-logging::log_buffer::log_buffer() : m_write_to_file(false) {}
+logging::log_buffer::log_buffer(bool log_to_console) : m_log_to_console(log_to_console), m_write_to_file(false) {}
 
-logging::log_buffer::log_buffer(const std::string &filename)
-    : m_write_to_file(true), m_file(filename, std::ios::out | std::ios::app) {
+logging::log_buffer::log_buffer(bool log_to_console, const std::string &filename)
+    : m_log_to_console(log_to_console), m_write_to_file(true), m_file(filename) {
     if (!m_file.is_open()) {
         throw std::runtime_error("Failed to open the log file");
     }
@@ -18,7 +20,9 @@ auto logging::log_buffer::overflow(int_type c) -> int_type {
     if (c == '\n') {
         m_is_new_line = true;
     }
-    m_cout_buffer->sputc(static_cast<char>(c));
+    if (m_log_to_console) {
+        m_cout_buffer->sputc(static_cast<char>(c));
+    }
     if (m_write_to_file) {
         m_file.put(static_cast<char>(c));
     }
@@ -27,7 +31,9 @@ auto logging::log_buffer::overflow(int_type c) -> int_type {
 
 int logging::log_buffer::sync() {
     //check_new_line();
-    m_cout_buffer->pubsync();
+    if (m_log_to_console) {
+        m_cout_buffer->pubsync();
+    }
     if (m_write_to_file) {
         m_file.flush();
     }
@@ -38,14 +44,30 @@ void logging::log_buffer::check_new_line() {
     if (!m_is_new_line) {
         return;
     }
-    std::ostringstream prefix_ss;
-    prefix_ss << "log: ";
-    std::string prefix = prefix_ss.str();
-    m_cout_buffer->sputn(prefix.c_str(), prefix.size());
+    std::string prefix = "log (" + get_time_str() + "): ";
+    if (m_log_to_console) {
+        m_cout_buffer->sputn(prefix.c_str(), prefix.size());
+    }
     if (m_write_to_file) {
         m_file.write(prefix.c_str(), prefix.size());
     }
     m_is_new_line = false;
+}
+
+#define adj(len) std::setfill('0') << std::setw(len)
+
+std::string logging::log_buffer::get_time_str() const {
+    std::ostringstream oss;
+
+    auto now = std::chrono::high_resolution_clock::now();
+    auto time_since_epoch = now.time_since_epoch();
+    auto h = std::chrono::duration_cast<std::chrono::hours>(time_since_epoch).count() % 24;
+    auto min = std::chrono::duration_cast<std::chrono::minutes>(time_since_epoch).count() % 60;
+    auto s = std::chrono::duration_cast<std::chrono::seconds>(time_since_epoch).count() % 60;
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(time_since_epoch).count() % 100;
+
+    oss << adj(2) << h << ":" << adj(2) << min << ":" << adj(2) << s << "." << adj(3) << ms;
+    return oss.str();
 }
 
 } // namespace dp
--- src/logging.hpp
+++ src/logging.hpp
@@ -8,31 +8,38 @@ namespace dp {
 
 class logging : public std::ostream {
 public:
-    logging() : std::ostream(&buffer) {}
-    logging(const std::string& filename) : std::ostream(&buffer), buffer(filename) {}
+    logging(bool log_to_console) : std::ostream(&buffer), buffer(log_to_console) {}
+    logging(bool log_to_console, const std::string& filename)
+        : std::ostream(&buffer), buffer(log_to_console, filename) {}
 
 private:
     class log_buffer : public std::streambuf {
     public:
-        log_buffer();
-        log_buffer(const std::string& filename);
+        log_buffer(bool log_to_console);
+        log_buffer(bool log_to_console, const std::string& filename);
 
     protected:
         int_type overflow(int_type c) override;
         int sync() override;
 
     private:
+        const bool m_log_to_console;
         const bool m_write_to_file;
         std::streambuf *m_cout_buffer {std::cout.rdbuf()};
         std::ofstream m_file;
         bool m_is_new_line {true};
 
         void check_new_line();
+        std::string get_time_str() const;
     };
 
     log_buffer buffer;
 };
 
-inline logging log {};
+#ifdef DEBUG
+inline logging log {true, "debug/dp.log"};
+#else
+inline logging log {false, "release/dp.log"};
+#endif
 
 } // namespace dp
