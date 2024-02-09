b6d9f953a581854babfc6b912db8cb38dd2349b3
Marek Zeleny <zeleny.marek@seznam.cz>
2024-02-09 15:48:21 +0100

    Add support for new values in LRU cache

--- src/data_structures/lru_cache.hpp
+++ src/data_structures/lru_cache.hpp
@@ -24,31 +24,40 @@ public:
         if (map_it == m_cache_map.end()) {
             return false;
         } else {
-            typename CacheList::iterator list_it = map_it->second;
-            output = std::get<1>(*list_it);
-            m_cache_list.push_front(*list_it);
-            m_cache_map[key] = m_cache_list.begin();
-            m_cache_list.erase(list_it);
+            output = std::get<1>(*map_it->second);
+            move_to_front(map_it);
             return true;
         }
     }
 
     void add(const Key &key, const T &entry) {
-        assert(!m_cache_map.contains(key));
-        m_cache_list.emplace_front(key, entry);
-        m_cache_map[key] = m_cache_list.begin();
-        if (size() > Capacity) {
-            Key removed_key = std::get<0>(m_cache_list.back());
-            [[maybe_unused]]
-            size_t removed = m_cache_map.erase(removed_key);
-            assert(removed == 1);
-            m_cache_list.pop_back();
+        typename CacheMap::iterator map_it = m_cache_map.find(key);
+        if (map_it != m_cache_map.end()) {
+            std::get<1>(*map_it->second) = entry;
+            move_to_front(map_it);
+        } else {
+            m_cache_list.emplace_front(key, entry);
+            m_cache_map[key] = m_cache_list.begin();
+            if (size() > Capacity) {
+                Key removed_key = std::get<0>(m_cache_list.back());
+                [[maybe_unused]]
+                size_t removed = m_cache_map.erase(removed_key);
+                assert(removed == 1);
+                m_cache_list.pop_back();
+            }
         }
     }
 
 private:
     CacheList m_cache_list;
     CacheMap m_cache_map;
+
+    void move_to_front(const typename CacheMap::iterator &map_it) {
+        typename CacheList::iterator list_it = map_it->second;
+        m_cache_list.push_front(*list_it);
+        m_cache_map[map_it->first] = m_cache_list.begin();
+        m_cache_list.erase(list_it);
+    }
 };
 
 } // namespace dp
