22a23713c5fe2ea6902c92d8b448b3989d7a8bc5
Marek Zeleny <zeleny.marek@seznam.cz>
2024-03-10 21:41:02 +0100

    Adjust logging for unit tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_session.hpp>
 #include <sylvan_obj.hpp>
+#include <simple_logger.h>
 
 TASK_2(int, tests, int, argc, char**, argv) {
     sylvan::Sylvan::initPackage(1LL<<22, 1LL<<26, 1LL<<22, 1LL<<26);
@@ -12,6 +13,7 @@ TASK_2(int, tests, int, argc, char**, argv) {
 }
 
 int main( int argc, char* argv[] ) {
+    simple_logger::SimpleLoggerConfig::logFileName = "tests.log";
     lace_start(0, 0);
     return RUN(tests, argc, argv);
 }
