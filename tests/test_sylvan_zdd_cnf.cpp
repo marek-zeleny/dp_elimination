5a9396fa7f1c3a2d38a29704549538f1fe97e64c
Marek Zeleny <zeleny.marek@seznam.cz>
2024-03-17 23:44:37 +0100

    Refactor ZDD CNF unit tests

--- tests/test_sylvan_zdd_cnf.cpp
+++ tests/test_sylvan_zdd_cnf.cpp
@@ -13,77 +13,129 @@ TEST_CASE("SylvanZddCnf operations", "[SylvanZddCnf]") {
     std::vector<SylvanZddCnf::Clause> clauses = {clause1, clause2};
 
     SECTION("Construction from vector") {
-        SylvanZddCnf cnf = SylvanZddCnf::from_vector(clauses);
-        REQUIRE_FALSE(cnf.is_empty());
-        REQUIRE(cnf.clauses_count() == 2);
+        SECTION("Simple case") {
+            SylvanZddCnf cnf = SylvanZddCnf::from_vector({
+                {1, -2, 3},
+                {-1, 2, -3},
+            });
+            CHECK_FALSE(cnf.is_empty());
+            CHECK(cnf.clauses_count() == 2);
+        }
 
-        SylvanZddCnf cnf1 = SylvanZddCnf::from_vector({{1, -2, 3}});
-        SylvanZddCnf cnf2 = SylvanZddCnf::from_vector({{-2, 3, 1}});
-        SylvanZddCnf cnf3 = SylvanZddCnf::from_vector({{3, -2, 1}});
-        REQUIRE_FALSE(cnf1.is_empty());
-        REQUIRE(cnf1.clauses_count() == 1);
-        REQUIRE(cnf1 == cnf2);
-        REQUIRE(cnf1 == cnf3);
+        SECTION("Order of variables doesn't matter") {
+            SylvanZddCnf cnf1 = SylvanZddCnf::from_vector({{1, -2, 3}});
+            SylvanZddCnf cnf2 = SylvanZddCnf::from_vector({{-2, 3, 1}});
+            SylvanZddCnf cnf3 = SylvanZddCnf::from_vector({{3, -2, 1}});
+            CHECK_FALSE(cnf1.is_empty());
+            CHECK(cnf1.clauses_count() == 1);
+            CHECK(cnf1 == cnf2);
+            CHECK(cnf1 == cnf3);
+        }
+
+        SECTION("Order of clauses doesn't matter") {
+            SylvanZddCnf cnf1 = SylvanZddCnf::from_vector({{1}, {-2}, {3}});
+            SylvanZddCnf cnf2 = SylvanZddCnf::from_vector({{-2}, {3}, {1}});
+            SylvanZddCnf cnf3 = SylvanZddCnf::from_vector({{3}, {-2}, {1}});
+            CHECK_FALSE(cnf1.is_empty());
+            CHECK(cnf1.clauses_count() == 3);
+            CHECK(cnf1 == cnf2);
+            CHECK(cnf1 == cnf3);
+        }
     }
 
     SECTION("Conversion to vector") {
+        std::vector<SylvanZddCnf::Clause> clauses = {
+                {1, -2, 3},
+                {-1, 2, -3},
+        };
         SylvanZddCnf cnf = SylvanZddCnf::from_vector(clauses);
         auto vector = cnf.to_vector();
         std::sort(vector.begin(), vector.end());
-        auto expected = clauses;
-        std::sort(expected.begin(), expected.end());
-        REQUIRE(vector == expected);
+        std::sort(clauses.begin(), clauses.end());
+        CHECK(vector == clauses);
     }
 
     SECTION("Equality operator") {
-        SylvanZddCnf cnf1 = SylvanZddCnf::from_vector(clauses);
-        SylvanZddCnf cnf2 = SylvanZddCnf::from_vector({clause2, clause1});
+        SylvanZddCnf cnf1 = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+            {-1, 2, -3},
+        });
+        SylvanZddCnf cnf2 = SylvanZddCnf::from_vector({
+            {-1, 2, -3},
+            {1, -2, 3},
+        });
         SylvanZddCnf cnf3 = cnf1;
         SylvanZddCnf empty_cnf = SylvanZddCnf::from_vector({});
-        SylvanZddCnf different_cnf = SylvanZddCnf::from_vector({clause1});
+        SylvanZddCnf different_cnf = SylvanZddCnf::from_vector({
+            {-1, 2, -3},
+        });
 
-        REQUIRE(cnf1 == cnf1);
-        REQUIRE(empty_cnf == empty_cnf);
-        REQUIRE(different_cnf == different_cnf);
+        SECTION("Reflectiveness") {
+            CHECK(cnf1 == cnf1);
+            CHECK(empty_cnf == empty_cnf);
+            CHECK(different_cnf == different_cnf);
+        }
 
-        REQUIRE(cnf1 == cnf2);
-        REQUIRE(cnf1 == cnf3);
-        REQUIRE(cnf2 == cnf3);
-        REQUIRE(cnf1 != different_cnf);
-        REQUIRE(cnf1 != empty_cnf);
-        REQUIRE(different_cnf != empty_cnf);
+        SECTION("Formulas with identical clauses are equal") {
+            CHECK(cnf1 == cnf2);
+            CHECK(cnf1 == cnf3);
+            CHECK(cnf2 == cnf3);
+        }
+
+        SECTION("Formulas with different clauses are not equal") {
+            CHECK(cnf1 != different_cnf);
+            CHECK(cnf1 != empty_cnf);
+            CHECK(different_cnf != empty_cnf);
+        }
     }
 
     SECTION("Property queries") {
-        SylvanZddCnf cnf = SylvanZddCnf::from_vector(clauses);
+        SylvanZddCnf cnf = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+            {-1, 2, -3},
+        });
         SylvanZddCnf empty_cnf1 = SylvanZddCnf::from_vector({});
         SylvanZddCnf empty_cnf2;
         SylvanZddCnf cnf_with_empty_clause1 = SylvanZddCnf::from_vector({{}});
-        SylvanZddCnf cnf_with_empty_clause2 = SylvanZddCnf::from_vector({clause1, clause2, {}});
+        SylvanZddCnf cnf_with_empty_clause2 = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+            {-1, 2, -3},
+            {},
+        });
 
-        REQUIRE_FALSE(cnf.is_empty());
-        REQUIRE_FALSE(cnf.contains_empty());
+        SECTION("Empty formulas") {
+            CHECK(empty_cnf1 == empty_cnf2);
 
-        REQUIRE(empty_cnf1.is_empty());
-        REQUIRE(empty_cnf2.is_empty());
-        REQUIRE_FALSE(empty_cnf1.contains_empty());
-        REQUIRE_FALSE(empty_cnf2.contains_empty());
-        REQUIRE(empty_cnf1 == empty_cnf2);
+            CHECK(empty_cnf1.is_empty());
+            CHECK(empty_cnf2.is_empty());
 
-        REQUIRE_FALSE(cnf_with_empty_clause1.is_empty());
-        REQUIRE_FALSE(cnf_with_empty_clause2.is_empty());
-        REQUIRE(cnf_with_empty_clause1.contains_empty());
-        REQUIRE(cnf_with_empty_clause2.contains_empty());
+            CHECK_FALSE(cnf.is_empty());
+            CHECK_FALSE(cnf_with_empty_clause1.is_empty());
+            CHECK_FALSE(cnf_with_empty_clause2.is_empty());
+        }
 
-        REQUIRE(cnf.get_smallest_variable() == 1);
-        REQUIRE(cnf.get_largest_variable() == 3);
-        REQUIRE(cnf_with_empty_clause2.get_smallest_variable() == 1);
-        REQUIRE(cnf_with_empty_clause2.get_largest_variable() == 3);
+        SECTION("Empty clauses") {
+            CHECK(cnf_with_empty_clause1.contains_empty());
+            CHECK(cnf_with_empty_clause2.contains_empty());
 
-        REQUIRE(empty_cnf1.get_smallest_variable() == 0);
-        REQUIRE(empty_cnf1.get_largest_variable() == 0);
-        REQUIRE(cnf_with_empty_clause1.get_smallest_variable() == 0);
-        REQUIRE(cnf_with_empty_clause1.get_largest_variable() == 0);
+            CHECK_FALSE(cnf.contains_empty());
+            CHECK_FALSE(empty_cnf1.contains_empty());
+            CHECK_FALSE(empty_cnf2.contains_empty());
+        }
+
+        SECTION("Smallest and largest variables") {
+            CHECK(cnf.get_smallest_variable() == 1);
+            CHECK(cnf.get_largest_variable() == 3);
+            CHECK(cnf_with_empty_clause2.get_smallest_variable() == 1);
+            CHECK(cnf_with_empty_clause2.get_largest_variable() == 3);
+        }
+
+        SECTION("Empty or zero clauses have no extreme variables") {
+            CHECK(empty_cnf1.get_smallest_variable() == 0);
+            CHECK(empty_cnf1.get_largest_variable() == 0);
+            CHECK(cnf_with_empty_clause1.get_smallest_variable() == 0);
+            CHECK(cnf_with_empty_clause1.get_largest_variable() == 0);
+        }
     }
 
     SECTION("Unit and clear literal detection") {
@@ -109,27 +161,31 @@ TEST_CASE("SylvanZddCnf operations", "[SylvanZddCnf]") {
         SylvanZddCnf::Literal unit;
         SylvanZddCnf::Literal clear;
 
-        unit = empty_cnf.get_unit_literal();
-        REQUIRE(unit == 0);
-        unit = cnf_with_empty_clause1.get_unit_literal();
-        REQUIRE(unit == 0);
-        unit = cnf_with_empty_clause2.get_unit_literal();
-        REQUIRE(unit == 1);
-        unit = cnf_with_multiple.get_unit_literal();
-        REQUIRE((unit == 1 || unit == 3));
-        unit = cnf_without_any.get_unit_literal();
-        REQUIRE(unit == 0);
+        SECTION("Unit literals") {
+            unit = empty_cnf.get_unit_literal();
+            CHECK(unit == 0);
+            unit = cnf_with_empty_clause1.get_unit_literal();
+            CHECK(unit == 0);
+            unit = cnf_with_empty_clause2.get_unit_literal();
+            CHECK(unit == 1);
+            unit = cnf_with_multiple.get_unit_literal();
+            CHECK((unit == 1 || unit == 3));
+            unit = cnf_without_any.get_unit_literal();
+            CHECK(unit == 0);
+        }
 
-        clear = empty_cnf.get_clear_literal();
-        REQUIRE(clear == 0);
-        clear = cnf_with_empty_clause1.get_clear_literal();
-        REQUIRE(clear == 0);
-        clear = cnf_with_empty_clause2.get_clear_literal();
-        REQUIRE(clear == 2);
-        clear = cnf_with_multiple.get_clear_literal();
-        REQUIRE((clear == 2 || clear == 4));
-        clear = cnf_without_any.get_clear_literal();
-        REQUIRE(clear == 0);
+        SECTION("Clear literals") {
+            clear = empty_cnf.get_clear_literal();
+            CHECK(clear == 0);
+            clear = cnf_with_empty_clause1.get_clear_literal();
+            CHECK(clear == 0);
+            clear = cnf_with_empty_clause2.get_clear_literal();
+            CHECK(clear == 2);
+            clear = cnf_with_multiple.get_clear_literal();
+            CHECK((clear == 2 || clear == 4));
+            clear = cnf_without_any.get_clear_literal();
+            CHECK(clear == 0);
+        }
     }
 
     SECTION("Formula statistics collection") {
@@ -171,67 +227,137 @@ TEST_CASE("SylvanZddCnf operations", "[SylvanZddCnf]") {
     }
 
     SECTION("Set operations") {
-        SylvanZddCnf cnf = SylvanZddCnf::from_vector(clauses);
+        SylvanZddCnf cnf = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+            {-1, 2, -3},
+        });
         SylvanZddCnf expected;
 
-        auto subset0 = cnf.subset0(-1);
-        expected = SylvanZddCnf::from_vector({clause1});
-        REQUIRE(subset0 == expected);
+        SECTION("Subset0") {
+            auto result1 = cnf.subset0(-1);
+            expected = SylvanZddCnf::from_vector({
+                {1, -2, 3},
+            });
+            CHECK(result1 == expected);
 
-        subset0 = cnf.subset0(1);
-        expected = SylvanZddCnf::from_vector({clause2});
-        REQUIRE(subset0 == expected);
+            auto result2 = cnf.subset0(1);
+            expected = SylvanZddCnf::from_vector({
+                {-1, 2, -3},
+            });
+            CHECK(result2 == expected);
+        }
 
-        auto subset1 = cnf.subset1(-1);
-        expected = SylvanZddCnf::from_vector({{2, -3}});
-        REQUIRE(subset1 == expected);
+        SECTION("Subset1") {
+            auto result1 = cnf.subset1(-1);
+            expected = SylvanZddCnf::from_vector({
+                {2, -3},
+            });
+            CHECK(result1 == expected);
 
-        subset1 = cnf.subset1(1);
-        expected = SylvanZddCnf::from_vector({{-2, 3}});
-        REQUIRE(subset1 == expected);
+            auto result2 = cnf.subset1(1);
+            expected = SylvanZddCnf::from_vector({
+                {-2, 3},
+            });
+            CHECK(result2 == expected);
+        }
 
-        auto unified = cnf.unify(subset1);
-        expected = SylvanZddCnf::from_vector({clause1, clause2, {-2, 3}});
-        REQUIRE(unified == expected);
+        SylvanZddCnf operand1 = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+        });
+        SylvanZddCnf operand2 = SylvanZddCnf::from_vector({
+            {-2, 3},
+        });
 
-        auto intersected = cnf.intersect(subset0);
-        expected = SylvanZddCnf::from_vector({clause2});
-        REQUIRE(intersected == expected);
+        SECTION("Union") {
+            auto result1 = cnf.unify(operand1);
+            CHECK(result1 == cnf);
 
-        auto subtracted = cnf.subtract(subset0);
-        expected = SylvanZddCnf::from_vector({clause1});
-        REQUIRE(subtracted == expected);
+            auto result2 = cnf.unify(operand2);
+            expected = SylvanZddCnf::from_vector({
+                {1, -2, 3},
+                {-1, 2, -3},
+                {-2, 3},
+            });
+            CHECK(result2 == expected);
+        }
 
-        auto multiplied = cnf.multiply(subset1);
-        expected = SylvanZddCnf::from_vector({clause1, {-1, 2, -2, 3, -3}});
-        REQUIRE(multiplied == expected);
+        SECTION("Intersection") {
+            auto result1 = cnf.intersect(operand1);
+            CHECK(result1 == operand1);
+
+            auto result2 = cnf.intersect(operand2);
+            expected = SylvanZddCnf();
+            CHECK(result2 == expected);
+        }
+
+        SECTION("Subtraction") {
+            auto result1 = cnf.subtract(operand1);
+            expected = SylvanZddCnf::from_vector({
+                {-1, 2, -3},
+            });
+            CHECK(result1 == expected);
+
+            auto result2 = cnf.subtract(operand2);
+            CHECK(result2 == cnf);
+        }
+
+        SECTION("Multiplication") {
+            auto result = cnf.multiply(operand2);
+            expected = SylvanZddCnf::from_vector({
+                {1, -2, 3},
+                {-1, 2, -2, 3, -3},
+            });
+            CHECK(result == expected);
+        }
     }
 
     SECTION("Removing tautologies") {
-        SylvanZddCnf cnf = SylvanZddCnf::from_vector({{}, {1, -1}, {-1, 2, 3}, {-2, 3, -3}});
-
+        SylvanZddCnf cnf = SylvanZddCnf::from_vector({
+            {},
+            {1, -1},
+            {-1, 2, 3},
+            {-2, 3, -3},
+        });
         auto no_tautologies = cnf.remove_tautologies();
-        auto expected = SylvanZddCnf::from_vector({{}, {-1, 2, 3}});
-        REQUIRE(no_tautologies == expected);
+        auto expected = SylvanZddCnf::from_vector({
+            {},
+            {-1, 2, 3},
+        });
+        CHECK(no_tautologies == expected);
     }
 
     SECTION("Removing subsumed clauses") {
-        SylvanZddCnf cnf = SylvanZddCnf::from_vector({{1, -2, 3}, {-1}, {-2, 3}, {-1, -2, -3}});
-
+        SylvanZddCnf cnf = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+            {-1},
+            {-2, 3},
+            {-1, -2, -3},
+        });
         auto no_subsumed = cnf.remove_subsumed_clauses();
-        auto expected = SylvanZddCnf::from_vector({{-1}, {-2, 3}});
-        REQUIRE(no_subsumed == expected);
+        auto expected = SylvanZddCnf::from_vector({
+            {-1},
+            {-2, 3},
+        });
+        CHECK(no_subsumed == expected);
     }
 
     SECTION("File operations") {
-        SylvanZddCnf cnf = SylvanZddCnf::from_vector(clauses);
+        SylvanZddCnf cnf = SylvanZddCnf::from_vector({
+            {1, -2, 3},
+            {-1, 2, -3},
+        });
+        std::string dot_file_name = ".test_zdd.dot";
+        std::string dimacs_file_name = ".test_dimacs.cnf";
 
-        std::string dot_file_name = "test_zdd.dot";
-        REQUIRE(cnf.draw_to_file(dot_file_name));
+        SECTION("Draw as dot file") {
+            CHECK(cnf.draw_to_file(dot_file_name));
+        }
+
+        SECTION("Write as DIMACS file") {
+            CHECK(cnf.write_dimacs_to_file(dimacs_file_name));
+        }
+
         std::filesystem::remove(dot_file_name.c_str());
-
-        std::string dimacs_file_name = "test_dimacs.cnf";
-        REQUIRE(cnf.write_dimacs_to_file(dimacs_file_name));
         std::filesystem::remove(dimacs_file_name.c_str());
     }
 }
