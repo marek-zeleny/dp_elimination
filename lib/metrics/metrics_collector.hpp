9cdef2d97ac0bc671c46cd15e54cbe21c7eea256
Marek Zeleny <zeleny.marek@seznam.cz>
2024-03-22 00:26:34 +0100

    Improve metrics collector

--- lib/metrics/metrics_collector.hpp
+++ lib/metrics/metrics_collector.hpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <vector>
 #include <chrono>
+#include <stdexcept>
 #include "utils.hpp"
 
 namespace dp {
@@ -15,31 +16,11 @@ concept IsEnumWithLast = IsEnum<Enum> && is_valid_value_v<Enum::Last>;
 template<IsEnumWithLast CounterEntries, IsEnumWithLast DurationEntries>
 class MetricsCollector {
 private:
-    using counter = size_t;
-    using clock = std::chrono::steady_clock;
-    using duration = clock::duration;
-    using durations = std::vector<duration>;
-    using counter_collection = std::array<counter, to_underlying(CounterEntries::Last) + 1>;
-    using durations_collection = std::array<durations, to_underlying(DurationEntries::Last) + 1>;
+    class Timer;
 
 public:
-    class Timer {
-        Timer(MetricsCollector &metrics, DurationEntries entry) :
-            m_collector(metrics), m_entry(entry), m_start_time(clock::now()) {}
-
-        ~Timer() {
-            duration elapsed_time = clock::now() - m_start_time;
-            m_collector.add_duration(m_entry, elapsed_time);
-        }
-
-    private:
-        MetricsCollector &m_collector;
-        const DurationEntries m_entry;
-        const clock::time_point m_start_time;
-    };
-
-    void counter_inc(CounterEntries entry) {
-        ++m_counters[to_underlying(entry)];
+    void increase_counter(CounterEntries entry, size_t amount = 1) {
+        m_counters[to_underlying(entry)] += amount;
     }
 
     [[nodiscard]] Timer get_timer(DurationEntries entry) {
@@ -47,6 +28,45 @@ public:
     }
 
 private:
+    using counter = size_t;
+    using clock = std::chrono::steady_clock;
+    using duration = clock::duration;
+    using durations = std::vector<duration>;
+    using counter_collection = std::array<counter, to_underlying(CounterEntries::Last) + 1>;
+    using durations_collection = std::array<durations, to_underlying(DurationEntries::Last) + 1>;
+
+    class Timer {
+    public:
+        Timer(MetricsCollector &metrics, DurationEntries entry) :
+                m_collector(metrics), m_entry(entry), m_start_time(clock::now()) {}
+
+        ~Timer() {
+            if (m_running) {
+                stop_impl();
+            }
+        }
+
+        void stop() {
+            if (m_running) {
+                stop_impl();
+            } else {
+                throw std::logic_error("Trying to stop a timer that has already stopped");
+            }
+        }
+
+    private:
+        MetricsCollector &m_collector;
+        const DurationEntries m_entry;
+        const clock::time_point m_start_time;
+        bool m_running{true};
+
+        void stop_impl() {
+            duration elapsed_time = clock::now() - m_start_time;
+            m_collector.add_duration(m_entry, elapsed_time);
+            m_running = false;
+        }
+    };
+
     counter_collection m_counters{};
     durations_collection m_durations{};
 
