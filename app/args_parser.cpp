0430ca121f2cd7b1fa0796d950c9e0eb79a0c561
Marek Zeleny <zeleny.marek@seznam.cz>
2024-03-23 13:54:38 +0100

    Improve app's help message

--- app/args_parser.cpp
+++ app/args_parser.cpp
@@ -11,8 +11,8 @@ static const std::unordered_map<std::string, ArgsParser::Heuristic> heuristic_ma
 std::optional<ArgsParser> ArgsParser::parse(int argc, char *argv[]) {
     ArgsParser args;
     CLI::App app("Davis Putnam elimination algorithm for preprocessing CNF formulas");
+    app.get_formatter()->column_width(40);
     app.option_defaults()->always_capture_default();
-    argv = app.ensure_utf8(argv);
 
     app.set_config("--config", "data/config.toml",
                    "Read a config file (.ini or .toml); precedence from the last if multiple");
@@ -20,27 +20,38 @@ std::optional<ArgsParser> ArgsParser::parse(int argc, char *argv[]) {
 
     app.add_option("-i,--input-file", args.m_input_cnf_file_name,
                    "File containing the input formula in DIMACS format")
+                   ->group("Files")
                    ->required();
     app.add_option("-o,--output-file", args.m_output_cnf_file_name,
-                   "File for writing the formula after variable elimination");
-    app.add_option("-l,--log-file", args.m_log_file_name,
-                   "File for writing logs");
+                   "File for writing the formula after variable elimination")
+                   ->group("Files");
     app.add_option("-m,--metrics-file", args.m_metrics_file_name,
-                   "File for exporting metrics (JSON)");
+                   "File for exporting metrics (JSON)")
+                   ->group("Files");
+    app.add_option("-l,--log-file", args.m_log_file_name,
+                   "File for writing logs")
+                   ->group("Files");
+
     app.add_option("--heuristic", args.m_heuristic,
                    "Heuristic for selecting eliminated literals")
+                   ->group("Algorithm")
                    ->transform(CLI::CheckedTransformer(heuristic_map,
                                                        CLI::ignore_case,
                                                        CLI::ignore_space,
                                                        CLI::ignore_underscore));
     app.add_option("-a,--absorbed-clause-elimination-interval", args.m_absorbed_clause_elimination_interval,
-                   "Number of eliminated variables before absorbed clauses are removed (never if 0)");
+                   "Number of eliminated variables before absorbed clauses are removed (never if 0)")
+                   ->group("Algorithm");
     app.add_option("-v,--var-range", args.m_var_range,
-                   "Range of variables that are allowed to be eliminated");
+                   "Range of variables that are allowed to be eliminated")
+                   ->group("Algorithm");
+
     app.add_option("--sylvan-table-size", args.m_sylvan_table_size,
-                   "Sylvan table size (default and max), should be a power of 2");
+                   "Sylvan table size (default and max), should be a power of 2")
+                   ->group("Sylvan");
     app.add_option("--sylvan-cache-size", args.m_sylvan_cache_size,
-                   "Sylvan cache size(default and max), should be a power of 2");
+                   "Sylvan cache size (default and max), should be a power of 2")
+                   ->group("Sylvan");
 
     try {
         app.parse(argc, argv);
