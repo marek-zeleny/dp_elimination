d48bee4596a7851a67b4c3473b35f0e8d0d2fd43
Marek Zeleny <zeleny.marek@seznam.cz>
2024-06-24 23:26:21 +0200

    Fix data collection for summary plots, improve labels

--- experiments/summary_plots.py
+++ experiments/summary_plots.py
@@ -33,13 +33,12 @@ def plot_durations(labels: list, data: dict[list]) -> tuple[plt.Figure, list[plt
     ax.set_title("Duration")
     ax.set_xlabel("input formulas")
     ax.set_ylabel(f"duration ({unit})")
-    ax.xaxis.set_ticklabels(labels)
+    ax.xaxis.set_ticks(range(len(labels)), labels, rotation=70)
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(get_divider(factor)))
     ax.set_ylim(bottom=0)
 
     fig.legend(loc="lower left")
     fig.tight_layout()
-    fig.subplots_adjust(bottom=0.1 * (len(data) // 2))
     return fig, axes
 
 
@@ -55,12 +54,11 @@ def plot_vars(labels: list, data: dict[list]) -> tuple[plt.Figure, list[plt.Axes
     ax.set_title("Remaining variables to eliminate")
     ax.set_xlabel("input formulas")
     ax.set_ylabel(f"% remaining variables")
-    ax.xaxis.set_ticklabels(labels)
+    ax.xaxis.set_ticks(range(len(labels)), labels, rotation=70)
     ax.set_ylim(bottom=0, top=1)
 
     fig.legend(loc="lower left")
     fig.tight_layout()
-    fig.subplots_adjust(bottom=0.1 * (len(data) // 2))
     return fig, axes
 
 
@@ -76,12 +74,12 @@ def plot_growth(labels: list, data: dict[list]) -> tuple[plt.Figure, list[plt.Ax
     ax.set_title("Formula growth")
     ax.set_xlabel("input formulas")
     ax.set_ylabel("relative growth of # clauses")
-    ax.xaxis.set_ticklabels(labels)
-    ax.set_ylim(bottom=0, top=1)
+    ax.xaxis.set_ticks(range(len(labels)), labels, rotation=70)
+    ax.set_yscale("log")
+    ax.set_ylim(bottom=0.4)
 
     fig.legend(loc="lower left")
     fig.tight_layout()
-    fig.subplots_adjust(bottom=0.1 * (len(data) // 2))
     return fig, axes
 
 
@@ -108,23 +106,24 @@ def prepare_setup_summary_data(data: dict[str, dict[str, tuple[float, float, flo
     for _, i_data in sorted(data.items()):
         for s in setups:
             duration, vars, growth = i_data.get(s, (float("nan"), float("nan"), float("nan")))
-            complete_data["duration"][s] = duration
-            complete_data["vars"][s] = vars
-            complete_data["growth"][s] = growth
+            complete_data["duration"][s].append(duration)
+            complete_data["vars"][s].append(vars)
+            complete_data["growth"][s].append(growth)
     return inputs, complete_data
 
 
 def create_setup_summary_plots(data: dict[str, dict[str, tuple[float, float, float]]]) -> list[tuple[str, plt.Figure]]:
     inputs, complete_data = prepare_setup_summary_data(data)
+    labels = [i.rsplit('/', 1)[1] for i in inputs]
     figures = []
 
-    fig_duration, _ = plot_durations(inputs, complete_data["duration"])
+    fig_duration, _ = plot_durations(labels, complete_data["duration"])
     figures.append(("duration", fig_duration))
 
-    fig_vars, _ = plot_vars(inputs, complete_data["vars"])
+    fig_vars, _ = plot_vars(labels, complete_data["vars"])
     figures.append(("variables", fig_vars))
 
-    fig_growth, _ = plot_growth(inputs, complete_data["growth"])
+    fig_growth, _ = plot_growth(labels, complete_data["growth"])
     figures.append(("growth", fig_growth))
 
     return figures
