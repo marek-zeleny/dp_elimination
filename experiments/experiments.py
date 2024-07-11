3c1817c1a5e4b4d3dcf99b00a99ed0ea9744522d
Marek Zeleny <zeleny.marek@seznam.cz>
2024-07-11 17:16:08 +0200

    Refuctor experiments run command to standalone file

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -2,16 +2,13 @@
 
 import os
 import sys
-import subprocess
-import time
 import argparse
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor
-from datetime import timedelta
 from typing import Callable, Generator
+from run import run_dp_experiments
 from tables import create_tables
 from plots import create_plots
 from summary_plots import extract_setup_summary_data, create_setup_summary_plots
@@ -23,7 +20,7 @@ default_config_path: Path = script_root_dir / "default_config.toml"
 inputs_dir: Path = script_root_dir / "inputs"
 setups_dir: Path = script_root_dir / "setups"
 
-# experiments
+# setups
 input_formulas: list[str] = None
 experiment_setups: list[str] = [
     "default",
@@ -53,89 +50,13 @@ def count_setups(args):
     print(len(list(generate_setups(Path()))))
 
 
-def run_experiment(command: list[str], cwd: Path, is_run_in_parallel: bool = True) -> int:
-    if is_run_in_parallel:
-        out_file = open(cwd / "out.txt", "w")
-        err_file = out_file
-    else:
-        out_file = sys.stdout
-        err_file = sys.stderr
-
-    def print_out(*args, **kwargs):
-        print(*args, **kwargs, file=out_file)
-
-    def print_err(*args, **kwargs):
-        print(*args, **kwargs, file=err_file)
-
-    print_out(" ".join(command))
-    print_out()
-    print_out("output:", flush=True)
-
-    start_time = time.monotonic()
-    result: subprocess.CompletedProcess[str] = subprocess.run(command, cwd=cwd, stdout=out_file, stderr=err_file)
-    end_time = time.monotonic()
-    duration = timedelta(seconds=end_time - start_time)
-
-    print_out()
-    print_out(f"Command exited with code {result.returncode}")
-
-    block = "=" * 10
-    if result.returncode == 0:
-        result_msg = "Experiment finished"
-    else:
-        result_msg = "Experiment failed"
-    print_out(f"{block} {result_msg}, runtime {duration} {block}", flush=True)
-
-    if out_file is not sys.stdout:
-        out_file.flush()
-        out_file.close()
-    return result.returncode
-
-
-def run_dp_experiments(args):
+# execution
+def run_experiments(args):
     dp_path = Path(args.dp_executable)
-    if not dp_path.exists():
-        print(f"Invalid path to dp executable: {dp_path}", file=sys.stderr)
-        sys.exit(1)
-    results_dir = Path(args.results_dir).absolute()
-    # prepare execution setups
-    setup_index = args.setup_index
-    commands: list[list[str]] = []
-    working_dirs: list[Path] = []
-    for i, (_, _,
-            setup_config_path,
-            input_config_path,
-            input_formula_path,
-            output_dir_path) in enumerate(generate_setups(results_dir)):
-        if setup_index >= 0 and i != setup_index:
-            continue
-        command_with_args = [str(dp_path.absolute()),
-                             str(input_formula_path),
-                             "--config", str(default_config_path),
-                             "--config", str(setup_config_path),
-        ]
-        if input_config_path.exists():
-            command_with_args += ["--config", str(input_config_path)]
-        os.makedirs(output_dir_path, exist_ok=True)
-        commands.append(command_with_args)
-        working_dirs.append(output_dir_path)
-    # execute in parallel
+    results_dir = Path(args.results_dir)
     num_processes = args.processes
-    if num_processes == 1:
-        in_parallel = [False for _ in range(len(commands))]
-        print(f"Running {len(commands)} experiments serially\n")
-    else:
-        assert num_processes > 1
-        assert len(commands) > num_processes
-        in_parallel = [True for _ in range(len(commands))]
-        print(f"Running {len(commands)} experiments in parallel (max {num_processes} processes)\n")
-    with ThreadPoolExecutor(max_workers=num_processes) as exec:
-        exit_codes = exec.map(run_experiment, commands, working_dirs, in_parallel)
-    # print results summary
-    if num_processes > 1:
-        print()
-        for dir, code in zip(working_dirs, exit_codes):
-            print(f"Experiment {dir.name} exited with code {code}")
+    setup_index = args.setup_index
+    run_dp_experiments(dp_path, results_dir, num_processes, setup_index)
 
 
 # data processing template
@@ -155,6 +76,7 @@ def process_metrics(results_dir_path: str, func: Callable[[Path, dict], None]):
             print(f"Metrics file {metrics_path} doesn't exist", file=sys.stderr, flush=True)
 
 
+# data processing
 def summarize_metrics(args):
     results_dir = args.results_dir
     format = args.format
@@ -223,7 +145,7 @@ parser_count.set_defaults(func=count_setups)
 parser_run = subparsers.add_parser("run",
                                    description="Run experiments",
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_run.set_defaults(func=run_dp_experiments)
+parser_run.set_defaults(func=run_experiments)
 parser_run.add_argument("dp_executable", type=str, help="Path to the compiled DP executable")
 parser_run.add_argument("-r", "--results-dir", type=str, default="results", help="Directory for storing results")
 parser_run.add_argument("-p", "--processes", type=int, default=1, help="Number of processes spawned concurrently")
---
+++ experiments/run.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import subprocess
+import time
+from concurrent.futures import ThreadPoolExecutor
+from datetime import timedelta
+from pathlib import Path
+from experiments import generate_setups, default_config_path
+
+
+def run_experiment(command: list[str], cwd: Path, is_run_in_parallel: bool = True) -> int:
+    if is_run_in_parallel:
+        out_file = open(cwd / "out.txt", "w")
+        err_file = out_file
+    else:
+        out_file = sys.stdout
+        err_file = sys.stderr
+
+    def print_out(*args, **kwargs):
+        print(*args, **kwargs, file=out_file)
+
+    def print_err(*args, **kwargs):
+        print(*args, **kwargs, file=err_file)
+
+    print_out(" ".join(command))
+    print_out()
+    print_out("output:", flush=True)
+
+    start_time = time.monotonic()
+    result: subprocess.CompletedProcess[str] = subprocess.run(command, cwd=cwd, stdout=out_file, stderr=err_file)
+    end_time = time.monotonic()
+    duration = timedelta(seconds=end_time - start_time)
+
+    print_out()
+    print_out(f"Command exited with code {result.returncode}")
+
+    block = "=" * 10
+    if result.returncode == 0:
+        result_msg = "Experiment finished"
+    else:
+        result_msg = "Experiment failed"
+    print_out(f"{block} {result_msg}, runtime {duration} {block}", flush=True)
+
+    if out_file is not sys.stdout:
+        out_file.flush()
+        out_file.close()
+    return result.returncode
+
+
+def run_dp_experiments(dp_path: Path, results_dir: Path, num_processes: int, setup_index: int):
+    if not dp_path.exists():
+        print(f"Invalid path to dp executable: {dp_path}", file=sys.stderr)
+        sys.exit(1)
+    # prepare execution setups
+    commands: list[list[str]] = []
+    working_dirs: list[Path] = []
+    for i, (_, _,
+            setup_config_path,
+            input_config_path,
+            input_formula_path,
+            output_dir_path) in enumerate(generate_setups(results_dir.absolute())):
+        if setup_index >= 0 and i != setup_index:
+            continue
+        command_with_args = [str(dp_path.absolute()),
+                             str(input_formula_path),
+                             "--config", str(default_config_path),
+                             "--config", str(setup_config_path),
+        ]
+        if input_config_path.exists():
+            command_with_args += ["--config", str(input_config_path)]
+        os.makedirs(output_dir_path, exist_ok=True)
+        commands.append(command_with_args)
+        working_dirs.append(output_dir_path)
+    # execute in parallel
+    if num_processes == 1:
+        in_parallel = [False for _ in range(len(commands))]
+        print(f"Running {len(commands)} experiments serially\n")
+    else:
+        assert num_processes > 1
+        assert len(commands) > num_processes
+        in_parallel = [True for _ in range(len(commands))]
+        print(f"Running {len(commands)} experiments in parallel (max {num_processes} processes)\n")
+    with ThreadPoolExecutor(max_workers=num_processes) as exec:
+        exit_codes = exec.map(run_experiment, commands, working_dirs, in_parallel)
+    # print results summary
+    if num_processes > 1:
+        print()
+        for dir, code in zip(working_dirs, exit_codes):
+            print(f"Experiment {dir.name} exited with code {code}")
