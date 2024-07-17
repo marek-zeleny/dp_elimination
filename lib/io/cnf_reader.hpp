edbf3d3829140a381ae6304df0ddc5f08b6e3295
Marek Zeleny <zeleny.marek@seznam.cz>
2024-07-18 01:05:17 +0200

    Add docstrings to IO

--- lib/io/cnf_reader.hpp
+++ lib/io/cnf_reader.hpp
@@ -8,8 +8,14 @@
 
 namespace dp {
 
+/**
+ * Static class encapsulating functions for reading CNF formula in the DIMACS CNF format.
+ */
 class CnfReader {
 public:
+    /**
+     * Error while reading a CNF formula.
+     */
     class failure : public std::runtime_error {
     public:
         failure(const std::string &what_arg, size_t line_num);
@@ -18,12 +24,26 @@ public:
         static std::string construct_msg(const std::string &msg, size_t line_num);
     };
 
+    /**
+     * Represents a literal of a propositional variable.
+     * Positive literals are positive numbers and vice versa.
+     * 0 is an invalid literal.
+     */
     using Literal = int32_t;
     using Clause = std::vector<Literal>;
     using AddClauseFunction = std::function<void(Clause &clause)>;
 
+    /**
+     * Reads a CNF formula from a given stream and calls given callback function for each parsed clause.
+     */
     static void read_from_stream(std::istream &stream, AddClauseFunction &func);
+    /**
+     * Reads a CNF formula from a given file and calls given callback function for each parsed clause.
+     */
     static void read_from_file(const std::string &file_name, AddClauseFunction &func);
+    /**
+     * Reads a CNF formula from a given file returns it as a vector of clauses.
+     */
     static std::vector<Clause> read_from_file_to_vector(const std::string &file_name);
 
 private:
--- lib/io/cnf_writer.hpp
+++ lib/io/cnf_writer.hpp
@@ -7,8 +7,14 @@
 
 namespace dp {
 
+/**
+ * Class for exporting CNF formulas in the DIMACS CNF format.
+ */
 class CnfWriter {
 public:
+    /**
+     * Error while writing a CNF formula.
+     */
     class failure : public std::runtime_error {
     public:
         explicit failure(const std::string &what_arg);
@@ -17,15 +23,45 @@ public:
         static std::string construct_msg(const std::string &msg);
     };
 
+    /**
+     * Represents a literal of a propositional variable.
+     * Positive literals are positive numbers and vice versa.
+     * 0 is an invalid literal.
+     */
     using Literal = int32_t;
     using Clause = std::vector<Literal>;
 
+    /**
+     * Writes a formula given as a vector of clauses to a given file.
+     */
     static void write_vector_to_file(const std::vector<Clause> &clauses, const std::string &file_name);
 
+    /**
+     * Creates a writer into a given stream.
+     *
+     * Upon instantiation writes a header into the stream.
+     *
+     * @param max_var Maximum variable allowed in clauses to be written.
+     * @param num_classes Number of clauses to be written.
+     */
     CnfWriter(std::ostream &output, size_t max_var, size_t num_classes);
+    /**
+     * Creates a writer into a given file.
+     *
+     * Upon instantiation writes a header into the file.
+     *
+     * @param max_var Maximum variable allowed in clauses to be written.
+     * @param num_classes Number of clauses to be written.
+     */
     CnfWriter(const std::string &file_name, size_t max_var, size_t num_clauses);
 
+    /**
+     * Writes next clause into the open stream.
+     */
     CnfWriter &write_clause(const Clause &clause);
+    /**
+     * Performs final checks, flushes and closes the stream.
+     */
     void finish();
 
 private:
