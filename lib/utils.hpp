d3e1be078dd256393ad0661ae9a3c13b3964080c
Marek Zeleny <zeleny.marek@seznam.cz>
2024-07-18 01:25:27 +0200

    Add docstrings to utils

--- lib/utils.hpp
+++ lib/utils.hpp
@@ -8,6 +8,10 @@
 namespace dp {
 
 // utilities for enum compile-time checks
+/**
+ * Checks if Value is a valid value.
+ * Useful for checking if an unknown enumeration type has a certain value.
+ */
 template<auto Value, typename = std::void_t<>>
 struct is_valid_value : std::false_type {};
 
@@ -17,22 +21,40 @@ struct is_valid_value<Value, std::void_t<decltype(Value)>> : std::true_type {};
 template<auto Value>
 constexpr bool is_valid_value_v = is_valid_value<Value>::value;
 
+/**
+ * Specifies that a type is an enumeration type.
+ */
 template<typename Enum>
 concept IsEnum = std::is_enum_v<Enum>;
 
+/**
+ * Converts an enumeration value to its underlying type.
+ */
 template<IsEnum Enum>
 constexpr std::underlying_type_t<Enum> to_underlying(Enum e) {
     return static_cast<std::underlying_type_t<Enum>>(e);
 }
 
-// based on the boost library (https://stackoverflow.com/a/2595226/11983817)
+/**
+ * Combines a given value's has with a given hash.
+ *
+ * Based on the boost library (https://stackoverflow.com/a/2595226/11983817)
+ *
+ * @param seed Preexisting hash.
+ * @param v Value who's hash is combined with the preexisting hash.
+ */
 template<typename T>
 void hash_combine(std::size_t &seed, const T &v) {
     std::hash<T> hash;
     seed ^= hash(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
-// based on https://stackoverflow.com/a/9248150/11983817
+
+/**
+ * Utility for printing tuples into a stream.
+ *
+ * Based on https://stackoverflow.com/a/9248150/11983817
+ */
 template<size_t N, typename... T>
 void print_tuple(std::ostream &os, const std::tuple<T...> &tup) {
     if constexpr (N < sizeof...(T)) {
