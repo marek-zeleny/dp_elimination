a138d920992ea6c12c89762e8322e621ce74750f
Marek Zeleny <zeleny.marek@seznam.cz>
2024-07-17 10:35:49 +0200

    Adjust summary plots to new table

--- experiments/summary_plots.py
+++ experiments/summary_plots.py
@@ -7,6 +7,36 @@ figure_height = 8
 bar_width = 0.7
 
 
+# data extraction
+def get_duration(df: pd.DataFrame, setups: str) -> dict[list]:
+    data = {
+        s: df.loc[:, (s, "duration")].values
+        for s in setups
+    }
+    return data
+
+
+def get_remaining_vars_ratio(df: pd.DataFrame, setups: str) -> dict[list]:
+    vars = df.loc[:, ("vars", "")]
+    aux_vars = df.loc[:, ("aux_vars", "")]
+    prot_vars = vars - aux_vars
+    data = {
+        s: ((df.loc[:, (s, "end_vars")] - prot_vars) / aux_vars).values
+        for s in setups
+    }
+    return data
+
+
+def get_relative_growth(df: pd.DataFrame, setups: str) -> dict[list]:
+    size = df.loc[:, ("size", "")]
+    data = {
+        s: (df.loc[:, (s, "end_size")] / size).values
+        for s in setups
+    }
+    return data
+
+
+#plots
 def _plot_bar_group(ax: plt.Axes, data: list[tuple[list, str]]):
     bars = len(data)
     w = bar_width / bars
@@ -85,22 +115,17 @@ def plot_growth(labels: list, data: dict[list]) -> tuple[plt.Figure, list[plt.Ax
 
 
 # interface
-def _get_data_for_metric(df: pd.DataFrame, setups: list[str], metric: str) -> dict[list]:
-    data = {setup: df.loc[:,(setup, metric)].values for setup in setups}
-    return data
-
-
 def create_setup_summary_plots(df: pd.DataFrame, setups: list[str]) -> list[tuple[str, plt.Figure]]:
     labels = [i.rsplit('/', 1)[1] for i in df.index]
     figures = []
 
-    fig_duration, _ = plot_durations(labels, _get_data_for_metric(df, setups, "duration"))
+    fig_duration, _ = plot_durations(labels, get_duration(df, setups))
     figures.append(("duration", fig_duration))
 
-    fig_vars, _ = plot_vars(labels, _get_data_for_metric(df, setups, "vars"))
+    fig_vars, _ = plot_vars(labels, get_remaining_vars_ratio(df, setups))
     figures.append(("variables", fig_vars))
 
-    fig_growth, _ = plot_growth(labels, _get_data_for_metric(df, setups, "growth"))
+    fig_growth, _ = plot_growth(labels, get_relative_growth(df, setups))
     figures.append(("growth", fig_growth))
 
     return figures
