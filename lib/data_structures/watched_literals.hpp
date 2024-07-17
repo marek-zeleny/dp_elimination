880c7617ccc4a03b88656a3265d88ce6a5f91079
Marek Zeleny <zeleny.marek@seznam.cz>
2024-07-17 19:54:19 +0200

    Add docstrings to WatchedLiterals

--- lib/data_structures/watched_literals.hpp
+++ lib/data_structures/watched_literals.hpp
@@ -7,8 +7,19 @@
 
 namespace dp {
 
+/**
+ * Watched literals data structure for unit propagation over CNF formulas.
+ *
+ * The data structure works on top of an existing set (or multiple sets) of clauses, it does not copy the formula.
+ * Make sure that the underlying data are valid as long as this instance is used.
+ */
 class WatchedLiterals {
 public:
+    /**
+     * Represents a literal of a propositional variable.
+     * Positive literals are positive numbers and vice versa.
+     * 0 is an invalid literal.
+     */
     using Literal = int32_t;
     using Clause = std::vector<Literal>;
 
@@ -16,27 +27,92 @@ public:
         unassigned = 0, negative = -1, positive = 1
     };
 
+    /**
+     * Creates an instance on top of a vector of clauses.
+     *
+     * @param max_var Maximum variable appearing in the set of clauses.
+     */
     WatchedLiterals(const std::vector<Clause> &clauses, size_t max_var);
+    /**
+     * Creates an instance on top of a vector of clauses, with some of the clauses deactivated by default.
+     * @param max_var Maximum variable appearing in the set of clauses.
+     */
     WatchedLiterals(const std::vector<Clause> &clauses, size_t max_var,
                     const std::unordered_set<size_t> &deactivated_clauses);
 
+    /**
+     * Creates an instance on top of a vector of clauses.
+     */
     static WatchedLiterals from_vector(const std::vector<Clause> &clauses);
+    /**
+     * Creates an instance on top of a vector of clauses, with some of the clauses deactivated by default.
+     */
     static WatchedLiterals from_vector(const std::vector<Clause> &clauses,
                                        const std::unordered_set<size_t> &deactivated_clauses);
+    /**
+     * Negates an assignment value ("unassigned" stays unchanged).
+     */
     static Assignment negate(const Assignment &a);
 
+    /**
+     * Adds a new clause.
+     *
+     * This causes a backtrack to 0th level.
+     * Propagates when finished.
+     */
     void add_clause(const Clause &clause, bool active = true);
+    /**
+     * Adds new clauses, with some of them deactivated by default.
+     *
+     * This causes a backtrack to 0th level.
+     * Propagates when finished.
+     */
     void add_clauses(const std::vector<Clause> &clauses, const std::unordered_set<size_t> &deactivated_clauses);
 
+    /**
+     * @return true if contradiction was derived.
+     */
     bool contains_empty() const;
+    /**
+     * @return current assignment level (number of explicitly assigned - not implied - variables).
+     */
     size_t get_assignment_level() const;
+    /**
+     * Assigns a literal to be true and propagates.
+     * @return true if propagation was successful, false if contradiction was derived.
+     */
     bool assign_value(Literal l);
+    /**
+     * @return Current assignment of given literal.
+     */
     Assignment get_assignment(Literal l) const;
+    /**
+     * Backtracks by a given number of levels.
+     *
+     * Throws if current assignment level is not high enough.
+     */
     void backtrack(size_t num_levels);
+    /**
+     * Backtracks to a given assignment level.
+     *
+     * Throws if the given level is higher than current assignment level.
+     */
     void backtrack_to(size_t target_level);
+    /**
+     * Activates and/or deactivates given clauses.
+     *
+     * This causes a backtrack to 0th level.
+     * Propagates when finished.
+     */
     void change_active_clauses(const std::vector<size_t> &activate_indices,
                                const std::vector<size_t> &deactivate_indices);
+    /**
+     * Prints clauses in the data structure and their current watches.
+     */
     void print_clauses(std::ostream &os = std::cout) const;
+    /**
+     * Prints current assignment stack.
+     */
     void print_stack(std::ostream &os = std::cout) const;
 
 private:
